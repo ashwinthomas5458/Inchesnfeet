07a08551b57830d5b7c175ef129049833347294d
ashwinthomas5458 <70959316+ashwinthomas5458@users.noreply.github.com>
2020-10-06 14:42:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ var portfoliothumb=document.querySelectorAll('.grey');
 portfoliothumb.forEach(thumb =>{
     thumb.addEventListener("click", ()=>{
         var source=thumb.alt;
-        var loc="https://ashwinthomas5458.github.io/portfolio/"+source;
+        var loc="https://ashwinthomas5458.github.io/Inchesnfeet/portfolio/"+source;
         window.location.href=loc;
     })
 })
