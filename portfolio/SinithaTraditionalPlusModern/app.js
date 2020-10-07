86a9314b38b5fdd216b525a668e5bd560c7b7180
Ashwin Thomas <ashwin.thomas.5458@gmail.com>
2020-10-07 09:46:01 +0530

    products page changed

--- portfolio/SinithaTraditionalPlusModern/app.js
+++ portfolio/SinithaTraditionalPlusModern/app.js
@@ -1,3 +1,29 @@
+const name=document.querySelector('header a');
+const carousel=document.querySelector('.carousel');
+const textbox=document.querySelector('.textcontainer');
+const button=document.querySelector('.btn');
+const container=document.querySelector('.imagecontainer');
+const thumbs=document.querySelector('.thumb');
+window.addEventListener('load', ()=>{
+    carousel.classList.add('carouselonload');
+    name.classList.add('inchesactive');
+    textbox.classList.add('textcontaineractive')
+})
+function imageview(){
+    button.addEventListener("click", ()=>{
+        carousel.classList.add('carouselactive');
+        container.classList.add('containeractive');
+        thumbs.classList.add('thumbactive');  
+    })
+    carousel.addEventListener("click", (r)=>{
+        if(r.target.classList.contains('carousel')){
+            carousel.classList.remove('carouselactive');
+            container.classList.remove('containeractive');
+            thumbs.classList.remove('thumbactive'); 
+        }
+    })
+}
+imageview();
 function imageclick(){
     const large = document.querySelector('.large');
     const thumb = document.querySelectorAll('.thumbnail')
