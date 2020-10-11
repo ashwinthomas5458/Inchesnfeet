589162fd5c956781c4a2d0f6d30f5375acbb5f8a
Ashwin Thomas <ashwin.thomas.5458@gmail.com>
2020-10-11 16:55:17 +0530

    about page added

--- about/app.js
+++ about/app.js
@@ -1,9 +1,37 @@
 const nav=document.querySelector('.nav');
+const navname=document.querySelector('.navname');
 const logo=document.querySelector('.logo');
-const covertext=document.querySelector('.covertext');
 window.addEventListener('load', ()=>{
     window.scrollTo(0,0)
+    navname.classList.add('navnameactive')
     nav.classList.add('navactive');
     logo.classList.add('logoactive');
-    covertext.classList.add('coveractive');
+})
+function scrollappear(object){
+    var objects=document.querySelectorAll(object);
+    var screenposition=window.innerHeight/1.2;
+    objects.forEach(obj =>{
+        var objposition=obj.getBoundingClientRect().top;
+        if(objposition<screenposition){
+        obj.classList.add('objectappear');
+        }
+        if(objposition>window.innerHeight){
+        obj.classList.remove('objectappear');
+        }
+    })
+}
+function navfade(){
+    var screenposition=window.innerHeight/2.1;
+    const navbar=document.querySelector('header');
+
+    if(window.pageYOffset<screenposition){
+        navbar.classList.remove('navbaractive');
+    }
+    if(window.pageYOffset>screenposition){
+        navbar.classList.add('navbaractive');
+    }
+}
+window.addEventListener('scroll', ()=>{
+    navfade();
+    scrollappear('.scrollevent');
 })
